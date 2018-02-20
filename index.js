8ac1bcf39a863823c3ff716252f164ad2e0cb3d9
SlavaD <slavad@sentinelone.com>
2018-02-20 16:31:39 +0200

    removed correct from answers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 var gcs = require('@google-cloud/storage')();
 
-exports.http = (request, response) => {
+exports.questions = (request, response) => {
   var file = gcs.bucket('loki-195808.appspot.com').file('/games/loki-games.json');
   var readStream = file.createReadStream();
   var file_content = '';
